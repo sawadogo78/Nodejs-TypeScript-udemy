f2816eab32453609b6b77e96c8a345d4990ac122
Tiga <sawadogotiga98@gmail.com>
2023-09-29 13:46:35 +0200

    Types Aliases and Interfaces

--- app.ts
+++ app.ts
@@ -6,7 +6,16 @@ const buttonElement = document.querySelector('button')!; // not null, the exclam
 const numResults: number[] = []; // this means an array full with number
 const textResults: string[] = []; // an array full with string
 
-function add(num1:number |string, num2:number|string) {
+// Type aliases and interfaces (this allow to optimize the code of types declaration)
+// Aliases
+type NumOrString = number |string;
+type Result = {val:number, timestamp:Date};
+//Interface (Not that you can uses interface or aliase to optimize element type declaration)
+interface ResultObject  {
+    val:number,
+    timestamp:Date
+}
+function add(num1:NumOrString, num2:NumOrString) {
     if(typeof num1==='number' && typeof num2==='number'){
         return num1 + num2; // number
     } else if(typeof num1==='string' && typeof num2==='string'){
@@ -15,9 +24,10 @@ function add(num1:number |string, num2:number|string) {
     return +num1+ +num2;// convert it in number
 }
 
-function printResult(resultObject:{val:number, timestamp:Date}){
+function printResult(resultObject:ResultObject){ // You can replace ResultObject (interface) with Result (aliase)
     console.log(resultObject.val);
 }
+
 
 buttonElement.addEventListener('click',()=>{
     const num1 = num1Element.value;
