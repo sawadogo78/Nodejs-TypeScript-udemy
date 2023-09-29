14fa559242893a526ed818fa6ca737a9ba0362a4
Tiga <sawadogotiga98@gmail.com>
2023-09-29 13:33:08 +0200

    Object and Array

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var num1Element = document.getElementById('num1');
 var num2Element = document.getElementById('num2');
 var buttonElement = document.querySelector('button'); // not null, the exclamtion
+// defining array
+var numResults = []; // this means an array full with number
+var textResults = []; // an array full with string
 function add(num1, num2) {
     if (typeof num1 === 'number' && typeof num2 === 'number') {
         return num1 + num2; // number
@@ -10,11 +13,18 @@ function add(num1, num2) {
     }
     return +num1 + +num2; // convert it in number
 }
+function printResult(resultObject) {
+    console.log(resultObject.val);
+}
 buttonElement.addEventListener('click', function () {
     var num1 = num1Element.value;
     var num2 = num2Element.value;
     var result = add(+num1, +num2); // put + to convert it to number
+    numResults.push(result); // adding element to typescript array
     var stringResult = add(num1, num2);
+    textResults.push(stringResult); //adding element to typescript array
     console.log(result);
     console.log(stringResult);
+    printResult({ val: +result, timestamp: new Date() });
+    console.log(numResults, textResults);
 });
--- app.ts
+++ app.ts
@@ -2,6 +2,10 @@ const num1Element = document.getElementById('num1') as HTMLInputElement;
 const num2Element = document.getElementById('num2') as HTMLInputElement;
 const buttonElement = document.querySelector('button')!; // not null, the exclamtion
 
+// defining array
+const numResults: number[] = []; // this means an array full with number
+const textResults: string[] = []; // an array full with string
+
 function add(num1:number |string, num2:number|string) {
     if(typeof num1==='number' && typeof num2==='number'){
         return num1 + num2; // number
@@ -11,13 +15,21 @@ function add(num1:number |string, num2:number|string) {
     return +num1+ +num2;// convert it in number
 }
 
+function printResult(resultObject:{val:number, timestamp:Date}){
+    console.log(resultObject.val);
+}
+
 buttonElement.addEventListener('click',()=>{
     const num1 = num1Element.value;
     const num2 = num2Element.value;
     const result = add(+num1, +num2); // put + to convert it to number
+    numResults.push(result as number); // adding element to typescript array
     const stringResult = add(num1, num2);
+    textResults.push(stringResult as string); //adding element to typescript array
     console.log(result);
     console.log(stringResult);
+    printResult({val:+result, timestamp: new Date()});
+    console.log(numResults, textResults);
 
 })
 
